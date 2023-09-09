a027ac813ff0fcaa63defe3ee277d0a4925f17b5
Divium <79859179+Divium-l@users.noreply.github.com>
2023-09-10 00:43:00 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 //
 // Created by Divium on 09/09/2023.
 //
+#include <iostream>
+#include "dynamic_array.hpp"
+
+struct Dummy {
+    inline static int count;
+    int id;
+    Dummy() {
+        id = count++;
+        std::cout << std::format("Dummy {} created", id) << std::endl;
+    }
+    ~Dummy() {
+        std::cout << std::format("Dummy {} DELETED", id) << std::endl;
+    }
+};
 
 int main() {
-   return 0;
+    auto arr = dvm::DynamicArray<Dummy>();
+
+    for (int i = 0; i < 10; i++) {
+        std::shared_ptr<Dummy> dummy(new Dummy());
+        arr.push_back(dummy);
+    }
+
+    for (int i = 0; i < 10; i++) {
+        arr.pop_back();
+    }
+
+    return 0;
 }
