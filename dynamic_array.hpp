b19984aff5da4c0b936680761c300e766cb75c2d
Divium <79859179+Divium-l@users.noreply.github.com>
2023-09-09 22:10:10 +0300

    Basic functionality

--- dynamic_array.hpp
+++ dynamic_array.hpp
@@ -1,43 +1,90 @@
 //
 // Created by Divium on 08/09/2023.
 //
+#pragma once
 
 #include <stdexcept>
 #include <memory>
 #include <format>
 
+/*
+ * If uncommenting, uncomment at the EOF!!!
+#define INITIAL_SIZE 5
+#define GROW_SIZE 5
+#define SHRINK_UNTIL 5
+#define SHRINK_THRESHOLD 5
+*/
 namespace dvm {
     /**
-        * @brief Dynamic array implementation using smart pointers
-    */
+     * @brief Dynamic array implementation using smart pointers
+     */
     template<class T> class DynamicArray {
     private:
         static const size_t INITIAL_SIZE = 5;
         static const size_t GROW_SIZE = 5;
-        static const size_t SHRINK_SIZE = 5;
-        static const size_t SHRINK_THRESHOLD = 5;
+        static const size_t SHRINK_UNTIL = 5;
+        static const size_t SHRINK_THRESHOLD = 10;
 
         size_t m_last_index;
         size_t m_size;
         std::unique_ptr<std::shared_ptr<T>[]> m_array;
 
+#pragma region "Utils"
         /**
-         * Grows array
-         * @param grow_by How much to grow
+         * Grows the array if capacity is reached by <i>GROW_SIZE</i>
+         * <br>
+         * <b>Call before adding elements</b>
          */
-        void _grow(size_t grow_by) {
-            m_size += grow_by;
+        void _try_grow() {
+            if (m_size - 1 < m_last_index)
+                return;
+            
+            _resize_to(m_size + GROW_SIZE);
+        }
+
+        /**
+         * Shrinks the array if number of empty cells more than <i>SHRINK_THRESHOLD</i> until there are <i>SHRINK_UNTIL</i> empty cells left.
+         * <br>
+         * <b>Call after removing elements</b>
+         */
+        void _try_shrink() {
+            auto empty_cells_count = m_size - 1 - m_last_index;
+            if (empty_cells_count <= SHRINK_THRESHOLD)
+                return;
+            
+            _resize_to(m_last_index + 1 + SHRINK_UNTIL);
+        }
+
+        /**
+         * Resizes the array to specified size and moves its data. Changes the <i>m_size</i> and <i>m_array</i>.
+         * @param size New array size
+         */
+        void _resize_to(size_t size) {
+            m_size = size;
             auto old_array = std::move(m_array);
-            auto new_array = _create_array(m_size);
+            auto new_array = _create_array();
 
             _copy(old_array, new_array);
 
             m_array = std::move(new_array);
         }
 
+        /**
+         * Shifts array elements to the left by one
+         * @param shift_from_index Index to be shifted from. Note: element at this index will be removed.
+         */
+        void _shift_left(size_t shift_from_index) {
+            for (size_t i = shift_from_index; i < m_last_index; i++)
+                m_array[i] = m_array[i + 1];
+        }
 
-        void _shrink() {
-
+        /**
+         * Shifts array elements to the right by one
+         * @param shift_from_index Index to be shifted from. Note: element at this index will be removed.
+         */
+        void _shift_right(size_t shift_from_index) {
+            for (size_t i = m_last_index; i > shift_from_index; i--)
+                m_array[i] = m_array[i - 1];
         }
 
         /**
@@ -59,9 +106,20 @@ namespace dvm {
                 new_array[i] = old_array[i];
         }
 
+        /**
+         * Asserts index is not out of bounds
+         * @param index Index
+         * @throws std::out_of_range When index is out of bounds
+         */
+        inline void _assert_bounds(size_t index) const {
+            if (index > m_last_index)
+                throw std::out_of_range(std::format("Index {} is out of bounds! Current array size is {}", index, m_last_index));
+        }
+
         [[deprecated]] void _delete(T *array) {
             delete[] array;
         }
+#pragma endregion "Utils"
 
     public:
         /**
@@ -73,43 +131,129 @@ namespace dvm {
             m_array = _create_array(m_size);
         }
 
+#pragma region "Access"
+        /**
+         * Returns shared pointer of requested element
+         * @param index Element location
+         * @return Shared pointer of requested element
+         * @throws std::out_of_range When index is out of range
+         */
+        std::shared_ptr<T> get(size_t index) const {
+            _assert_bounds(index);
+
+            return m_array[index];
+        }
+
         /**
          * Returns shared pointer of requested element
          * @param index Element location
          * @returns Shared pointer of requested element
          * @throws std::out_of_range When index is out of range
          */
-        std::shared_ptr<T> get(size_t index) const {
-            if (index > m_last_index)
-                throw std::out_of_range(std::format("Index {} is out of bounds! Current array size is {}", index, m_last_index));
-
-            return m_array[index];
+        std::shared_ptr<T> operator[](size_t index) const {
+            return get(index);
         }
 
+        /**
+         * @returns First element of the array. If array is empty, returns <b>nullptr</b>.
+         */
+        [[maybe_unused]] std::shared_ptr<T> first() const noexcept {
+            if (!is_empty())
+                return m_array[0];
+            else
+                return nullptr;
+        }
+
+        /**
+         * @returns Last element of the array. If array is empty, returns <b>nullptr</b>.
+         */
+        [[maybe_unused]] std::shared_ptr<T> last() const noexcept {
+            if (!is_empty())
+                return m_array[m_last_index];
+            else
+                return nullptr;
+        }
+#pragma endregion "Access"
+
+#pragma region "Modifiers"
         /**
          * Appends element at the end
          * @param object Element to add
          */
-        void push_back(std::shared_ptr<T> object) {
-            if (m_size - 1 >= m_last_index)
-                _grow(GROW_SIZE);
+        [[maybe_unused]] void push_back(std::shared_ptr<T> object) {
+            m_last_index++;
+            _try_grow();
 
-            m_array[++m_last_index] = object;
+            m_array[m_last_index] = object;
         }
 
+        /**
+         * Inserts element at the specified index
+         * @param index Index
+         * @param object Element
+         * @throws std::out_of_range When index is out of bounds
+         */
+        [[maybe_unused]] void insert(size_t index, std::shared_ptr<T> object) {
+            _assert_bounds();
+            m_last_index++;
+            _try_grow();
+
+            _shift_right(index);
+            m_array[index] = object;
+        }
+        
         /**
          * Removes last element
          */
-        void pop_back() {
-            m_array[m_last_index] = nullptr;
-            m_last_index--;
+        [[maybe_unused]] void pop_back() {
+            m_array[m_last_index--] = nullptr;
+            _try_shrink();
         }
 
         /**
-         * @return Array size
+         * Removes element at the specified index
+         * @param index Index
+         * @throws std::out_of_range When index is out of bounds
          */
-        [[nodiscard]] size_t size() const noexcept {
+        [[maybe_unused]] void remove(size_t index) {
+            _assert_bounds(index);
+
+            m_array[index] = nullptr;
+
+            _shift_left(index);
+            _try_shrink();
+        }
+#pragma endregion "Modifiers"
+
+#pragma region "Memory"
+        /**
+         * @return Number of elements
+         */
+        [[maybe_unused]] [[nodiscard]] size_t size() const noexcept {
             return m_last_index;
         }
+
+        /**
+         * @return The number of elements that can be held in currently allocated space
+         */
+        [[maybe_unused]] [[nodiscard]] size_t capacity() const noexcept {
+            return m_size;
+        }
+
+        /**
+         * Checks whether the array has no elements
+         * @return Boolean
+         */
+        [[maybe_unused]] [[nodiscard]] bool is_empty() const noexcept {
+            return m_last_index == 0;
+        }
+#pragma endregion "Memory"
     };
 }
+
+/*
+#undef INITIAL_SIZE
+#undef GROW_SIZE
+#undef SHRINK_UNTIL
+#undef SHRINK_THRESHOLD
+ */
