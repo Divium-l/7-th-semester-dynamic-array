91ff469341d31a13fbaf59936a85be9768d28477
Divium <79859179+Divium-l@users.noreply.github.com>
2023-12-14 18:08:33 +0300

    added interface and concept for it

--- containers/src/comparable.hpp
+++ containers/src/comparable.hpp
@@ -9,4 +9,24 @@ namespace dvm {
         object > object2;
         object < object2;
     };
+    template<class T> concept CIComparable = requires(T object, T object2) {
+        object.compare(object);
+    };
+
+    /**
+     * Interface for comparing
+     * @tparam T CComparable
+     */
+    template<class T> class IComparable {
+        /**
+         *
+         *  <ul>
+         *      <li>Must return -1 if lhs \<  rhs</li>
+         *      <li>Must return  0 if lhs == rhs</li>
+         *      <li>Must return  1 if lhs >  rhs</li>
+         * </ul>
+         * @return One of the following [-1, 0, 1]
+         */
+        virtual int8_t compare(T& lhs, T& rhs) = 0;
+    };
 }
